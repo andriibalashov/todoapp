57244033b6ec2fe6429436086bde9fe139fc2846
Andriy <andriybalashov@MacBook-Pro-4.local>
2016-08-01 01:59:31 -0600

    added libs

--- public/js/controllers/TodoController.js
+++ public/js/controllers/TodoController.js
@@ -1,5 +1,6 @@
 todoapp.controller('TodoController', ['$scope', '$http', 'users', 'todos', '$routeParams',
   function ($scope, $http, users, todos, $routeParams) {
-    //$scope.todoId=$routeParams.todoId;
-    //$scope.todo = {};
+    $scope.todoId=$routeParams.todoId;
+    $scope.todo = {};
+    
   }]);
